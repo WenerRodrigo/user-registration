2cad83778b229205afeb3419b53c6313b705a603
WenerRodrigo <wener19.wr@gmail.com>
2022-11-22 22:28:32 -0300

    finalizado rotas, estilos, e os componentes do usuário

---
+++ frontend/src/components/template/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Main from '../Main'
+
+export default props => 
+  <Main
+    icon="home"
+    title="Início"
+    subtitle="Segundo Projeto do capítulo react."
+  >
+    <div className="display-4">Bem Vindo</div>
+    <hr />
+    <p className="mb-0">
+      Sistema para exemplificar a construção de um cadastro desenvolvido em
+      React!
+    </p>
+  </Main>
+
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,18 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'font-awesome/css/font-awesome.min.css'
-import './App.css'
-import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import "./App.css";
+import React from "react";
 
-import Logo from '../components/template/Logo'
-import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
-import Footer from '../components/template/Footer'
+import { BrowserRouter } from "react-router-dom";
 
-export default props =>
+import Logo from "../components/template/Logo";
+import Nav from "../components/template/Nav";
+import Route from "./Route";
+import Footer from "../components/template/Footer";
+
+export default props => (
+  <BrowserRouter>
     <div className="app">
-        <Logo />
-        <Nav />
-        <Main icon="home" title="Início"
-        subtitle="Segundo Projeto do capítulo react."/>
-        <Footer />
+      <Logo />
+      <Nav />
+      <Route/>
+      <Footer />
     </div>
+  </BrowserRouter>
+);
